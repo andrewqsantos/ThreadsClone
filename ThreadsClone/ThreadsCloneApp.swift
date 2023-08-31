4378443ab658a79252a912b73fe8ee25ff57d231
Andrew Quenehen <andrewqsantos@pm.me>
2023-08-30 21:48:34 -0300

    Initial Commit

---
+++ ThreadsClone/ThreadsCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsCloneApp.swift
+//  ThreadsClone
+//
+//  Created by Andrew Quenehen on 30/08/23.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
